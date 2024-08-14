dc1015ef4c8c0cf0c62ce9eec82a35e2a6a3491e
Pokemon918 <j27394645@gmail.com>
2024-08-13 19:45:40 -0700

    add: memory

--- index.py
+++ index.py
@@ -4,7 +4,13 @@ load_dotenv(find_dotenv(), override=True)
 from langchain_openai import ChatOpenAI
 from langchain.schema import SystemMessage
 from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory
+
+memory = ConversationBufferMemory(
+    memory_key='chat_history',
+    return_messages=True
+)
 
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
 
@@ -12,6 +18,7 @@ prompt = ChatPromptTemplate(
     input_variables=['content'],
     messages=[
         SystemMessage(content='You are chatbot having a conversation with a human.'),
+        MessagesPlaceholder(variable_name='chat_history'),
         HumanMessagePromptTemplate.from_template('{content}')
     ]
 )
@@ -19,6 +26,7 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
     llm=llm,
     prompt=prompt,
+    memory=memory,
     verbose=False
 )
 
